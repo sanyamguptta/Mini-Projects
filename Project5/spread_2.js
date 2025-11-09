fb71182838b2f63a700a1856400b6a0c4cf1612d
Sanyam <sanyamg2018@gmail.com>
2025-11-09 11:09:42 +0530

    Add Project5 JavaScript project

---
+++ Project5/spread_2.js
@@ -0,0 +1,37 @@
+// spread
+
+var ar = [1, 2, 3, 4];
+var br = [...ar];
+
+
+//conditional-  if else else-if ternary operator switch case
+var age = 17
+if(age > 18) {
+    console.log("Adult")
+}
+else {
+    console.log("Child")
+}
+
+
+//truthy and falsy values :-  hr value ko true or false bnaya jaa skta h, agar voh truthy h toh true and agar falsy h toh false 
+// falsy value - null undefined NaN 0 ' ' false " "  document.all
+// baaki sab truthy values hoti h 
+// if(null) console.log('heyy')
+//     else console.log('byee')
+
+
+
+// lOOPS IN JS
+        //1. for loop
+for(var i=0; i<=10; i++) {
+    console.log('heyy')
+}
+
+        //2. while loop
+var a = 0
+while(a<6) {
+    console.log(a)
+    i++
+}
+
